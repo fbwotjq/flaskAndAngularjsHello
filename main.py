ea90b95cc9369f078126e157afed842c241fd61e
jaco <fbwotjq@gmail.com>
2016-08-08 13:46:09 +0900

    init commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, json
+
+app = Flask(__name__)
+#app.config['STATIC_FOLDER'] = 'static'
+
+@app.route("/")
+def root():
+    return "This is root page"
+
+@app.route("/justGet", methods=['GET'])
+def justGet():
+    returnData = json.dumps({'name':'fbwotjq', 'id': 'jaco.ryu', 'type':'GET'})
+    return returnData
+
+@app.route("/justPost", methods=['Post'])
+def justPost():
+    returnData = json.dumps({'name':'fbwotjq', 'id': 'jaco.ryu', 'type':'POST'})
+    return returnData
+
+if __name__ == "__main__":
+    app.run()
---
+++ readme.txt
@@ -0,0 +1,4 @@
+ * 셋팅 커멘드
+ - sudo pip install virtualenv
+ - virtualenv env
+ -  . env/bin/activate
